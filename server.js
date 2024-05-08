07d192228a3d2aec1dfd9f4652b7fa45e13005f5
Oke Omoge <omoge82@gmail.com>
2024-05-08 12:12:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express()
 
 //Routes
 app.get('/', (req, res)=> {
-    res.send('TodoList API by Moralist')
+    res.send('Product API by Moralist')
 })
 
 app.listen(3000, () => {
-    console.log('TodoList App listening on port 3000')
+    console.log('Product App listening on port 3000')
 })
 
